97cb7463f03173b329e66215528a8c6e7821da14
Edgars Skrabins <shiftkiro@gmail.com>
2024-01-21 17:31:14 +0200

    Refactored PlayerManager

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    private GameObject controller;
     private PhotonView PV;
 
-    private GameObject controller;
-
     private void Awake()
+    {
+        InitializeReferences();
+    }
+
+    private void Start()
+    {
+        InitializeMultiplayerSetup();
+    }
+
+    private void InitializeReferences()
     {
         PV = GetComponent<PhotonView>();
     }
 
-    private void Start()
+    private void InitializeMultiplayerSetup()
     {
         if (PV.IsMine)
         {
@@ -23,9 +32,9 @@ public class PlayerManager : MonoBehaviour
 
     private void CreateController()
     {
-        Transform spawnpoint = SpawnManager.I.GetSpawnpoint();
-        controller = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), spawnpoint.position,
-            spawnpoint.rotation, 0, new object[] { PV.ViewID });
+        Transform spawnPoint = SpawnManager.I.GetSpawnpoint();
+        controller = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), spawnPoint.position,
+            spawnPoint.rotation, 0, new object[] { PV.ViewID });
     }
 
     public void Die()
